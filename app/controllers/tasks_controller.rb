e4d7fbc216d2c6e3391654d83db82833d0b4ae40
Nikky Rojas <rojas7313@gmail.com>
2022-07-01 15:56:08 -0600

    first commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+    def index
+        @tasks = Task.all
+    end
+
+    def new
+    end
+
+    def create
+        binding.pry
+        task = Task.new({
+            title: params[:task][:title],
+            description: params[:task][:description]
+        })
+        binding.pry
+        task.save
+        binding.pry
+        redirect_to 'tasks'
+    end
+    def show
+        @task = Task.find(params[:id])
+    end
+
+    def edit
+        @task =Task.find(params[:id])
+    end
+
+    def update
+        task = Task.find(params[:id])
+        task.update({
+            title: params[:task][:title],
+            description: params[:task][:description]
+        })
+        task.save
+        redirect_to "/task/#{task.id}"
+    end
+
+    def destroy
+        Task.destroy(params[:id])
+        redirect_to '/tasks'
+    end
+end
